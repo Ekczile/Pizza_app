bdeffc280b34d5a3e733b472d1587f3754e0cefe
Steven <ekczile@gmail.com>
2018-11-20 21:23:58 -0500

    added delivery option, added new page to check info

--- app.rb
+++ app.rb
@@ -4,3 +4,19 @@ require_relative 'pizza.rb'
 get '/' do
     erb :home
 end
+
+post '/home' do
+    size = params[size]
+    meats = params[meat].join(",")
+    veg = params[veggies].join(",")
+    extras = params[extras].join(",")
+    redirect 'checkselected?size=' + size + '&meat=' + meat + '&veggies=' + veg + '&extras=' + extras
+end
+
+get '/checkselected' do
+    size = params[size]
+    meats = params[meat].split(",")
+    veg = params[veggies].split(",")
+    extras = params[extras].split(",")
+    erb :lastname, locals: {size: size, meats: meats, veg: veg, extras: extras}
+end
