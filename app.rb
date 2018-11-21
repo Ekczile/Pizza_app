d8158f00a8be92e92b557df3f22b37087a5717d6
Steven <ekczile@gmail.com>
2018-11-21 13:52:29 -0500

    added images to check pages and final pages

--- app.rb
+++ app.rb
@@ -7,36 +7,36 @@ end
 
 post '/home' do
     size = params[:size]
-    meats = params[:meat].join(", ")
-    veg = params[:veggies].join(", ")
-    extras = params[:extras].join(", ")
+    meats = params[:meat].join(",")
+    veg = params[:veggies].join(",")
+    extras = params[:extras].join(",")
     delivery = params[:delivery]
     redirect 'checkselected?size=' + size + '&meat=' + meats + '&veggies=' + veg + '&extras=' + extras + '&delivery=' + delivery
 end
 
 get '/checkselected' do
     size = params[:size]
-    meats = params[:meat].split(", ")
-    veg = params[:veggies].split(", ")
-    extras = params[:extras].split(", ")
+    meats = params[:meat].split(",")
+    veg = params[:veggies].split(",")
+    extras = params[:extras].split(",")
     delivery = params[:delivery]
     erb :checkselected, locals: {size: size, meats: meats, veg: veg, extras: extras, delivery: delivery}
 end
 
 post '/checkselected' do
     size = params[:size]
-    meats = params[:meat].join(", ")
-    veg = params[:veggies].join(", ")
-    extras = params[:extras].join(", ")
+    meats = params[:meat].join(",")
+    veg = params[:veggies].join(",")
+    extras = params[:extras].join(",")
     delivery = params[:delivery]
     redirect 'final?size=' + size + '&meat=' + meats + '&veggies=' + veg + '&extras=' + extras + '&delivery=' + delivery
 end
 
 get '/final' do
     size = params[:size]
-    meats = params[:meat].split(", ")
-    veg = params[:veggies].split(", ")
-    extras = params[:extras].split(", ")
+    meats = eval(params[:meat])
+    veg = eval(params[:veggies])
+    extras = eval(params[:extras])
     delivery = params[:delivery]
     erb :final, locals: {size: size, meats: meats, veg: veg, extras: extras, delivery: delivery}
 end
